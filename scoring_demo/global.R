425cf63ec97ed551f0a26d3e00685e548a446856
James Eddy <james.a.eddy@gmail.com>
2019-10-10 15:00:43 -0700

    initial commit of scoring app

---
+++ scoring_demo/global.R
@@ -0,0 +1,125 @@
+library(shiny)
+library(listviewer)
+library(DT)
+library(shinyjs)
+
+
+submission_data <- readr::read_file("../test_rembrandt.json") %>% 
+  jsonlite::fromJSON(simplifyVector = FALSE)
+submission_annotated <- readr::read_file("../annotated_rembrandt.json") %>% 
+  jsonlite::fromJSON(simplifyVector = FALSE)
+
+de_wt <- 1.0
+dec_wt <- 1.0
+top_wt <- 2.0
+vd_wt <- 0.5
+
+score_checks <- tribble(
+  ~step, ~check, ~nextIfTrue, ~nextIfFalse, ~pointsIfTrue,
+  1L, "Result matches Gold Standard?", 2L, 3L, de_wt,
+  2L, "Top Result = Gold Standard?", NA, 3L, top_wt,
+  3L, "'Good' DEC Concept Codes Match?", NA, 4L, dec_wt,
+  4L, "'Good' VD Coverage?", NA, 5L, vd_wt,
+  5L, "[HR] Better Match?", NA, NA, 1
+)
+
+jaccard <- function(list_a, list_b) {
+  n <- length(intersect(list_a, list_b))
+  u <- length(union(list_a, list_b))
+  n / u
+}
+
+get_dec_concepts <- function(col_data, res_num = 1) {
+  col_data$results[[res_num]]$result$dataElementConcept$concepts %>% 
+    flatten_chr() %>% 
+    unique()
+}
+
+get_observed_values <- function(col_data, res_num = 1) {
+  col_data$results[[res_num]]$observedValues %>% 
+    map(~ list(value = .$value, name = .$concept$name, id = .$concept$id)) %>% 
+    map(~ discard(., is.null)) %>% 
+    map_df(as_tibble)
+}
+
+
+get_res_score <- function(sub_data, anno_data, res_num) {
+  overlap_thresh <- 0.5
+  coverage_thresh <- 0.8
+
+  sub_c_ids <- get_dec_concepts(sub_data, res_num)
+  anno_c_ids <- get_dec_concepts(anno_data)
+  metric_3 <- jaccard(sub_c_ids, anno_c_ids)
+  
+  sub_ovs <- get_observed_values(sub_data, res_num)
+  anno_ovs <- get_observed_values(anno_data)
+  if ("id" %in% names(sub_ovs)) {
+    check_col <- "id"
+  } else {
+    check_col <- "name"
+  }
+  mismatch_rows <- find_mismatch_rows(sub_ovs, anno_ovs, check_col)
+  metric_4 <- 1 - (length(mismatch_rows) / nrow(anno_ovs))
+  
+  check_1 <- magrittr::equals(
+    sub_data$results[[res_num]]$result$dataElementConcept$id,
+    anno_data$results[[1]]$result$dataElementConcept$id
+  )
+  
+  check_2 <- NA
+  check_3 <- NA
+  check_4 <- NA
+  if (check_1) {
+    check_2 <- res_num == 1
+    if (!check_2) {
+      check_3 <- metric_3 > overlap_thresh
+    } 
+  } else {
+    check_3 <- metric_3 > overlap_thresh
+    
+    if (!check_3) {
+      check_4 <- metric_4 > coverage_thresh
+      
+    }
+  }
+  
+  score_1 <- check_1*score_checks$pointsIfTrue[[1]]
+  score_2 <- check_2*score_checks$pointsIfTrue[[2]]
+  score_3 <- check_3*metric_3*score_checks$pointsIfTrue[[3]]
+  score_4 <- check_4*metric_4*score_checks$pointsIfTrue[[4]]
+  
+  
+  list(step = 1:4,
+       metric = c(check_1, check_2, metric_3, metric_4),
+       status = c(check_1, check_2, check_3, check_4),
+       score = c(score_1, score_2, score_3, score_4))
+}
+
+# get_col_score <- function(sub_data, anno_data, num_res) {
+#   
+# }
+
+get_de_table <- function(col_data, res_num = 1) {
+  as_tibble(col_data$results[[res_num]]$result$dataElement)
+}
+
+get_dec_table <- function(col_data, res_num = 1) {
+  as_tibble(col_data$results[[res_num]]$result$dataElementConcept) %>% 
+    select(-concepts) %>% 
+    distinct()
+}
+
+find_mismatch_cols <- function(df_a, df_b) {
+  names(df_a) %>% 
+    imap(function(n, x) {
+      if (any(df_a[[x]] != df_b[[x]])) {
+        n
+      }
+    }) %>% 
+    discard(is.null) %>% 
+    flatten_chr()
+}
+
+find_mismatch_rows <- function(df_a, df_b, col_name = "id") {
+  df_a[[col_name]][!(df_a[[col_name]] %in% df_b[[col_name]])]
+}
---
+++ scoring_demo/ui.R
@@ -0,0 +1,235 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Metadata Automation Scoring"),
+    useShinyjs(),
+
+    sidebarLayout(
+        sidebarPanel(
+            width = 3,
+            selectInput("column",
+                        "Column number",
+                        choices = 1:8,
+                        selected = 1),
+            fluidRow(
+                column(
+                    width = 12,
+                    h5(
+                        strong("Header value:"), 
+                        code(textOutput("header_val", inline = TRUE))
+                    )
+                )
+            ),
+            hr(),
+
+                
+            h6(strong("Match Weightings")),
+            wellPanel(
+                div(
+                    style = "font-size:85%;text-align:center;",
+                    fluidRow(
+                        column(
+                            6,
+                            textInput("de_wt", "DE", 
+                                      value = 1.0, width = "100%")
+                        ),
+                        column(
+                            6,
+                            textInput("dec_wt", "DEC", 
+                                      value = 1.0, width = "100%")
+                        )
+                    ),
+                    fluidRow(
+                        column(
+                            12,
+                            textInput("top_wt", "Top Result", 
+                                      value = 2.0, width = "100%")
+                        )
+                    ),
+                    fluidRow(
+                        column(
+                            12,
+                            textInput("vd_wt", "VD Coverage", 
+                                      value = 0.5, width = "100%")
+                        )
+                    )
+                )
+            ),
+            h6(strong("Metric Cutoffs")),
+            wellPanel(
+                div(
+                    style = "font-size:85%;text-align:center;",
+                    fluidRow(
+                        column(
+                            12,
+                            textInput("overlap_thresh", "Overlap (Jaccard)", 
+                                      value = 0.5, width = "100%")
+                        )
+                    ),
+                    fluidRow(
+                        column(
+                            12,
+                            textInput("coverage_thresh", "VD Coverage", 
+                                      value = 0.8, width = "100%")
+                        )
+                    )
+                )
+            )
+
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            width = 9,
+            fluidRow(
+                wellPanel(
+                    fluidRow(
+                        column(
+                            4,
+                            uiOutput("result_opts")
+                        ),
+                        column(
+                            8,
+                            h5("Result score"),
+                            textOutput("res_score")
+                        )
+                    )
+                )
+            ),
+            fluidRow(
+                tabsetPanel(
+                    tabPanel(
+                        "Score Breakdown",
+                        fluidRow(
+                            column(
+                                12,
+                                tableOutput("score_table")
+                            )
+                        )
+                    ),
+                    tabPanel(
+                        "Score Procedure",
+                        fluidRow(
+                            column(
+                                12,
+                                tableOutput("score_proc_table")
+                            )
+                        )
+                    )
+                )
+            ),
+            br(), br(),
+            fluidRow(
+                tabsetPanel(
+                    tabPanel(
+                        "Header",
+                        br(),
+                        fluidRow(
+                            column(
+                                6,
+                                h4(strong("Submitted data"))
+                            ),
+                            column(
+                                6,
+                                h4(strong("Annotated data"))
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                12,
+                                h5(strong("Data Element (DE)"))
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                6,
+                                DTOutput("sub_de_table")
+                            ),
+                            column(
+                                6,
+                                DTOutput("anno_de_table")
+                            )
+                        ),
+                        br(), br(),
+                        fluidRow(
+                            column(
+                                12,
+                                h5(strong("Data Element Concept (DEC)"))
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                6,
+                                DTOutput("sub_dec_table"),
+                                DTOutput("sub_concepts", width = "50%"),
+                                br(),
+                                p(
+                                    strong("Concept Overlap:"), 
+                                    textOutput("concept_overlap", inline = TRUE)
+                                )
+                            ),
+                            column(
+                                6,
+                                DTOutput("anno_dec_table"),
+                                DTOutput("anno_concepts", width = "50%")
+                            )
+                        )
+                    ),
+                    tabPanel(
+                        "Values",
+                        br(),
+                        fluidRow(
+                            column(
+                                6,
+                                h4(strong("Submitted data"))
+                            ),
+                            column(
+                                6,
+                                h4(strong("Annotated data"))
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                12,
+                                h5(strong("Observed Values & Matches"))
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                6,
+                                DTOutput("sub_ovs"),
+                                br(),
+                                p(
+                                    strong("Value Domain Coverage:"), 
+                                    textOutput("ov_coverage", inline = TRUE)
+                                )
+                            ),
+                            column(
+                                6,
+                                DTOutput("anno_ovs")
+                            )
+                        )
+                        
+                    )
+                )
+            ),
+            hr(),
+            actionButton(
+                "show_json",
+                "Show/hide JSON"
+            ),
+            br(), br(),
+            jsoneditOutput("sub_json")
+        )
+    )
+))
