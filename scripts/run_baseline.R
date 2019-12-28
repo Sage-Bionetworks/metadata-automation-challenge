da44b845f1d8100221f66641677570e28fb1f6e9
James Eddy <james.a.eddy@gmail.com>
2019-12-27 16:59:25 -0800

    reorganize repo files

---
+++ scripts/run_baseline.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(fuzzyjoin)
+source("R/baseline_annotator.R")
+
+main <- function() {
+  args <- commandArgs(trailingOnly = TRUE)
+  dataset_name <- args[1]
+  message(glue::glue("Annotating input table for '{dset_name}' dataset",
+                     dset_name = dataset_name))
+  run_annotator(dataset_name)
+}
+
+main()
