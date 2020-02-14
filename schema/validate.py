8c9fd30ddf1561d30d92a74fa03fe11fcaa32525
verena <9377970+vpchung@users.noreply.github.com>
2020-02-14 11:23:39 -0800

    indicate that errors are found for APOLLO-2 but do not include details

--- schema/validate.py
+++ schema/validate.py
@@ -102,16 +102,19 @@ def validate_submission_tool(submission_file, schema_filepath,
         invalid_reasons = ['Expected FileEntity type but found ' + entity_type]
         dataset = ''
     else:
+        errors = _validate_json(submission_file, schema_filepath)
+        if errors:
+            prediction_file_status = "INVALID"
+            invalid_reasons.extend(errors)
+        else:
+            prediction_file_status = "VALIDATED"
         dataset = os.path.basename(
             submission_file).replace("-Submission.json", '')
-        prediction_file_status = "VALIDATED"
-        if dataset != "APOLLO-2":
-            errors = _validate_json(submission_file, schema_filepath)
-            if errors:
-                prediction_file_status = "INVALID"
-                invalid_reasons.extend(errors)
     if invalid_reasons:
-        invalid_string = "\n".join(invalid_reasons)
+        if dataset == "APOLLO-2":
+            invalid_string = "Errors found."
+        else:
+            invalid_string = "\n".join(invalid_reasons)
         error_string = '> {} ERRORS:\n{}\n'.format(dataset, invalid_string)
 
     else:
